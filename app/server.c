2c532bd11a62d070be9c43ff0386efa2ade37bde
Art <iwiivi@gmail.com>
2024-12-02 15:05:01 +0000

    Add support for 404 not founds;

--- app/server.c
+++ app/server.c
@@ -1,11 +1,24 @@
 #include <errno.h>
 #include <netinet/in.h>
 #include <netinet/ip.h>
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <sys/socket.h>
 #include <unistd.h>
+
+#define PORT 4221
+#define BUFFER_SIZE 1024
+
+#define SUCCESS_200 "HTTP/1.1 200 OK\r\n\r\n";
+
+// Example Request:
+// GET /index.html HTTP/1.1\r\n
+// Host: localhost:4221\r\n
+// User-Agent: curl/7.64.1\r\n
+// Accept: */*\r\n
+// \r\n
 
 int main() {
   // Disable output buffering
@@ -38,7 +51,7 @@ int main() {
 
   struct sockaddr_in serv_addr = {
       .sin_family = AF_INET,
-      .sin_port = htons(4221),
+      .sin_port = htons(PORT),
       .sin_addr = {htonl(INADDR_ANY)},
   };
 
@@ -56,13 +69,39 @@ int main() {
   printf("Waiting for a client to connect...\n");
   client_addr_len = sizeof(client_addr);
 
-  int fd = accept(server_fd, (struct sockaddr *)&client_addr,
-                  (unsigned int *)&client_addr_len);
-  printf("Client connected\n");
+  while (true) {
+    int client_fd = accept(server_fd, (struct sockaddr *)&client_addr,
+                           (unsigned int *)&client_addr_len);
+    printf("Client connected\n");
 
-  char *reply = "HTTP/1.1 200 OK\r\n\r\n";
+    // Handling of connection
+    // tofree = string = strdup("abc,def,ghi");
+    // assert(string != NULL);
+    char buffer[BUFFER_SIZE];
 
-  int bytes_sent = send(fd, reply, strlen(reply), 0);
+    int valread = read(client_fd, buffer, BUFFER_SIZE);
+
+    if (valread < 0) {
+      printf("Read failed: %s \n", strerror(errno));
+      return 1;
+    }
+
+    printf("Received:\r\n\"\"\"\r\n%s\"\"\"\r\n", buffer);
+    char method[16], path[256];
+
+    sscanf(buffer, "%s %s", method, path);
+
+    printf("METHOD: %s\n", method);
+    printf("PATH: %s\n", path);
+
+    char *reply;
+    if (strcmp(method, "/") == 0) {
+      reply = SUCCESS_200;
+    } else {
+      reply = "HTTP/1.1 404 NOT FOUND\r\n\r\n";
+    }
+    int bytes_sent = send(client_fd, &reply, strlen(reply), 0);
+  }
 
   close(server_fd);
 
